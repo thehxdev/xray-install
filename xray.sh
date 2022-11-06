bb095a46d5fc80ee377e08ed5d23194e8fb3cfd4
thehxdev <xclimax1324@gmail.com>
2022-11-06 22:21:46 +0330

    updated

--- xray.sh
+++ xray.sh
@@ -2,14 +2,14 @@
 
 # Colors
 Color_Off='\033[0m'
-Black='\033[0;30m' 
+#Black='\033[0;30m' 
 Red='\033[0;31m'   
 Green='\033[0;32m' 
 Yellow='\033[0;33m'
 Blue='\033[0;34m'  
-Purple='\033[0;35m'
+#Purple='\033[0;35m'
 Cyan='\033[0;36m'  
-White='\033[0;37m' 
+#White='\033[0;37m' 
 
 # Variables 
 #github_branch="main"
