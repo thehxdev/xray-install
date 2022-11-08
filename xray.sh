c36aa4da47b392290bc56474bc770add16b6dbab
thehxdev <xclimax1324@gmail.com>
2022-11-08 12:17:36 +0330

    updated

--- xray.sh
+++ xray.sh
@@ -1434,6 +1434,7 @@ function make_backup() {
 		mkdir /root/old_xray_backups/ >/dev/null 2>&1
 		mv ${backup_dir} /root/old_xray_backups/
 		judge "move existing backup to /root/old_xray_backups"
+		mkdir ${backup_dir} >/dev/null 2>&1
 		if [ -e "/root/xray_backup.tar.gz" ]; then
 		mv /root/xray_backup.tar.gz /root/old_xray_backups/
 		judge "move existing backup.tar.gz to /root/old_xray_backups"
